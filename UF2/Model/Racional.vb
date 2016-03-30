8dd8bdabe180d1686beec332fd0f5477db2b724d
David Pérez Orfila <knight_bcn@hotmail.com>
2016-03-30 18:40:43 +0200

    racional

---
+++ UF2/Model/Racional.vb
@@ -0,0 +1,29 @@
+﻿Public Class Racional
+    'Definimos las propiedades de la clase como private para que desde el exterior no se modifiquen'
+
+    Private numerador As Integer
+    Private denominador As Integer
+
+    'Se crea el método con la función pública new que será el constructor'
+
+    Public Sub New(ByVal numerador As Integer, ByVal denominador As Integer)
+        Me.numerador = numerador
+        Me.denominador = denominador
+
+    End Sub
+
+    'Se crea el método con la función pública new que será el segundo constructor'
+
+    Public Sub New()
+        Dim r As New Random
+        Me.numerador = r.Next(1, 10)
+        Me.denominador = r.Next(1, 10)
+    End Sub
+
+    'Se crea el método function multiplicar que devuelve un Racional'
+
+    Public Function multiplicar() As Racional
+
+    End Function
+
+End Class
